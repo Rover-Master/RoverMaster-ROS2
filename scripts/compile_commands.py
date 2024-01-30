3e463cc84425f29f821bc2da7e28553836ec2686
Yuxuan Zhang <zhangyuxuan@ufl.edu>
2024-01-30 20:53:44 +0000

    scripts/compile_commands.py: add documentations

--- scripts/compile_commands.py
+++ scripts/compile_commands.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+# ============================================================
+# This python3 script aggregates all compile_commands.json
+# files from build directories into a single file at the
+# workspace root folder.
+# It helps vscode-clangd extension to find all the headers
+# and provide better code hints and completion.
+# ============================================================
+# This script is supposed to be run from the workspace root.
+# Makefile will automatically run this after build completes.
+# ============================================================
+
 import json
 from glob import glob
 
@@ -8,7 +19,6 @@ for file in glob('build/**/compile_commands.json'):
     with open(file, 'r') as f:
         data = json.load(f)
         compile_commands.extend(data)
-        print(file)
 
 with open('compile_commands.json', 'w') as f:
     json.dump(compile_commands, f, indent=2)
