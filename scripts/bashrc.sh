bec2e24ba40cf488fe33a206c308f3ef1481ad3a
Yuxuan Zhang <zhangyuxuan@ufl.edu>
2024-01-30 23:21:39 +0000

    bashrc.sh: add `launch` function with auto completion

--- scripts/bashrc.sh
+++ scripts/bashrc.sh
@@ -17,3 +17,27 @@ fi
 # Rewrite the PS1 prompt to highlight current ROS2 environment
 export WS=$(pwd)
 export PS1="\033[01;32mROS2::$(basename $WS)\[\033[00m\] \[\033[01;34m\]\w\[\033[00m\]\$ "
+# Function to launch ROS2 manifests in ./launch/
+function launch() {
+    if [ -f launch/$1 ]; then
+        ros2 launch launch/$1
+    elif [ -f launch/$1.py ]; then
+        ros2 launch launch/$1.py
+    elif [ -f launch/$1.xml ]; then
+        ros2 launch launch/$1.xml
+    elif [ -f launch/$1.yml ]; then
+        ros2 launch launch/$1.yml
+    elif [ -f launch/$1.yaml ]; then
+        ros2 launch launch/$1.yaml
+    else
+        for file in launch/*; do
+            echo $(basename $file)
+        done
+    fi
+}
+# Add completion for launch function
+LAUNCH_FILES=
+for file in launch/*; do
+    LAUNCH_FILES+="$(basename $file) "
+done
+complete -W "$LAUNCH_FILES" launch
