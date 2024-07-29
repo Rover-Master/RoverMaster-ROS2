e23b64cba20781a3ef5352251424280a43abfcf3
Yuxuan Zhang <zyx@z-yx.cc>
2024-07-29 19:30:27 +0000

    update

--- src/mission/setup.py
+++ src/mission/setup.py
@@ -24,7 +24,7 @@ setup(
     tests_require=["pytest"],
     entry_points={
         "console_scripts": [
-            "main = mission.robot:main",
+            "main = mission.main:main",
         ],
     },
 )
