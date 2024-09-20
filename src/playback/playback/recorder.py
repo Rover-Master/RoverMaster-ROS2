c19c64ec0d978a78925b8a4326d7eba7da996069
Yuxuan Zhang <github.uf@z-yx.cc>
2024-09-20 22:11:52 +0000

    update: fix playback/recorder video writing issue

--- src/playback/playback/recorder.py
+++ src/playback/playback/recorder.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
-import cv2, rclpy
+import cv2, rclpy, sys
 from rclpy.time import Time
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
-from pathlib import Path
+
+
+# Test codecs according to preferences
+for cc in ["avc1", "mp4v", "MJPG", "XVID", "DIVX"]:
+    codec = cv2.VideoWriter_fourcc(*cc)
+    v_test = cv2.VideoWriter("/tmp/test.mp4", codec, 30, (640, 480), isColor=True)
+    success = v_test.isOpened()
+    v_test.release()
+    if success:
+        break
+    else:
+        cc = None
+        codec = None
 
 
 class Recorder(Node):
@@ -17,8 +29,13 @@ class Recorder(Node):
 
     def callback(self, msg: Image):
         ts = Time.from_msg(msg.header.stamp)
-        frame = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
-        self.get_logger().info(f"Received Image: {ts}")
+        # Video writer only works with color images.
+        frame = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        if frame.shape[2] == 1:
+            frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
+        if frame.shape[2] == 4:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
+        self.get_logger().info(f"Frame: {frame.shape} - {ts.nanoseconds / 1e9:.4f}s")
         # Initialize the video writer
         if self.first_frame is None or self.first_ts is None:
             self.first_frame = frame
@@ -29,11 +46,10 @@ class Recorder(Node):
             if dt <= 1e-3:
                 dt = 1 / 30
             fps = round(1e9 / dt, 2)
-            codec = cv2.VideoWriter_fourcc(*"mjpg")
             size = self.first_frame.shape[:2][::-1]
             # Log the information
-            self.get_logger().info(f"Recording at {fps} FPS and size {size}")
-            self.video = cv2.VideoWriter(self.dst, codec, fps, size)
+            self.get_logger().info(f"Recording at {fps} FPS, size {size}, codec {cc}")
+            self.video = cv2.VideoWriter(self.dst, codec, fps, size, isColor=True)
             # Push the first frame
             self.video.write(self.first_frame)
         else:
@@ -51,11 +67,6 @@ class Recorder(Node):
         self.sub = self.create_subscription(Image, src, self.callback, 10)
         self.get_logger().info(f"Saving to file {self.dst}")
 
-    def destory_node(self):
-        if self.video is not None:
-            self.video.release()
-        super().destory_node()
-
 
 # Main
 def main():
@@ -65,4 +76,7 @@ def main():
         rclpy.spin(node)
     except KeyboardInterrupt:
         pass
+    print("Closing video writer", file=sys.stderr)
+    if node.video is not None:
+        node.video.release()
     node.destroy_node()
