fd5e3f5d87264bc9c00ccaae28ba79905f8c1a82
Yuxuan Zhang <github.uf@z-yx.cc>
2024-09-24 02:01:45 +0000

    update Make scripts: fix python env issue

---
+++ scripts/clear-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# ============================================================
+# Clears all environment variables in the current shell.
+# ============================================================
+# Author: Yuxuan Zhang
+# Email : robotics@z-yx.cc
+# License: MIT
+# ============================================================
+unset PATH
+source /etc/environment
+source /etc/profile
---
+++ scripts/shell.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# ============================================================
+# This script is used to setup interactive shell environment.
+# It is intended for Make scripts, do not run directly.
+# ============================================================
+# Author: Yuxuan Zhang
+# Email : robotics@z-yx.cc
+# License: MIT
+# ============================================================
+DIVIDER="============================================================"
+echo -e "\e[90m${DIVIDER}\e[0m"
+# Display banner (optional)
+if [ ! -z "${BANNER}" ]; then
+    echo -e "\e[34m>>>>>> \e[0;90m \e[0;1;36m${BANNER}\e[0;90m"
+    unset BANNER
+fi
+# Source user environment file, if exists
+if [ -f ~/.bashrc ]; then
+    source ~/.bashrc
+elif [ -f ~/.bash_profile ]; then
+    source ~/.bash_profile
+elif [ -f ~/.profile ]; then
+    source ~/.profile
+fi
+# Check for ROS2 installation
+source scripts/ros-env.sh
+# Activate Conda environment if exists
+source scripts/conda-env.sh
+# Command to find PWD
+CMD_PWD='$('"pwd | sed 's@^${ROS_WS}\?@.@'"')'
+# Rewrite the PS1 prompt to highlight current ROS2 environment
+export PS1="\033[034mROS2::${ROS_DISTRO}\[\033[00m\]"
+export PS1="${PS1} \[\033[04;32m\]$(basename $ROS_WS)\[\033[00m\]"
+export PS1="${PS1} \[\033[96m\]${CMD_PWD}\[\033[00m\] \$ "
+# Function to launch ROS2 manifests in ./launch/
+function launch() {
+    if [ -f launch/$1 ]; then
+        ros2 launch launch/$1
+    elif [ -f launch/$1.py ]; then
+        ros2 launch launch/$1.py
+    elif [ -f launch/$1.xml ]; then
+        ros2 launch launch/$1.xml
+    elif [ -f launch/$1.yml ]; then
+        ros2 launch launch/$1.yml
+    elif [ -f launch/$1.yaml ]; then
+        ros2 launch launch/$1.yaml
+    else
+        echo "========================"
+        if [ -z "$1" ]; then
+            echo "Usage: launch <file>"
+            echo "========================"
+        else
+            echo "File not found: $1"
+            echo "========================"
+        fi
+        echo "Available launch files:"
+        for file in launch/*; do
+            echo "- $(basename $file)"
+        done
+        echo
+    fi
+}
+# Add completion for launch function
+LAUNCH_FILES=
+for file in launch/*; do
+    LAUNCH_FILES+="$(basename $file) "
+done
+complete -W "$LAUNCH_FILES" launch
+# Source additional environment files if exists
+for file in *.sh; do
+    if [ -f "$file" ]; then
+        echo -e "\e[33m[INFO] \e[0;90m Sourcing additional env: \e[4m$file\e[0m"
+        source $file
+    fi
+done
+echo -e "\e[90m${DIVIDER}\e[0m"
