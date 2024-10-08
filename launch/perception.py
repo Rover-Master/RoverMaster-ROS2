d810e218ecf26e1389d544f4ed5ba2bdfaea775b
Yuxuan Zhang <zyx@z-yx.cc>
2024-10-08 15:49:38 -0400

    update launch/perception: auto encode video upon task completion

--- launch/perception.py
+++ launch/perception.py
@@ -7,12 +7,13 @@ from launch.substitutions import LocalSubstitution
 from os import environ as env
 from pathlib import Path
 from datetime import datetime
-import os
+import os, sys, atexit
 
 
-run_id = datetime.now().strftime('%Y%m%d_%H%M%S')
-PWD = Path(env['PWD'])
-RUN_VAR = str(PWD / 'var' / run_id)
+run_id = datetime.now().strftime("%Y%m%d_%H%M%S")
+PWD = Path(env["PWD"])
+RUN_VAR = PWD / "var" / run_id
+IMAGES_DIR = str(RUN_VAR / "perception_images")
 nodes = [
     Node(
         package="rover",
@@ -35,6 +36,8 @@ nodes = [
         remappings=[
             ("image_in", "/spinnaker/camera_0/img"),
             ("motion", "/rover/base/velocity/set"),
+            ("halt", "/rover/base/halt"),
+            ("imu", "/rover/base/imu"),
         ],
     ),
     Node(
@@ -42,80 +45,48 @@ nodes = [
         executable="recorder",
         parameters=[
             {"src": "/perception/image_out"},
-            {"dst": RUN_VAR + "/perception_images"},
+            {"dst": IMAGES_DIR},
         ],
     ),
     Node(
-        package='sllidar_ros2',
-        executable='sllidar_node',
-        parameters=[{
-            'channel_type':'serial',
-            'serial_port': '/dev/rplidar', 
-            'serial_baudrate': 115200, 
-            'frame_id': 'laser',
-            'inverted': False, 
-            'angle_compensate': True,
-        }],
-        output='screen'
+        package="sllidar_ros2",
+        executable="sllidar_node",
+        parameters=[
+            {
+                "channel_type": "serial",
+                "serial_port": "/dev/rplidar",
+                "serial_baudrate": 115200,
+                "frame_id": "laser",
+                "inverted": False,
+                "angle_compensate": True,
+            }
+        ],
+        output="screen",
     ),
-    Node(
-        package='lidar_toolbox',
-        executable='scan_transformer',
-        output='screen'
-    ),
-    Node(
-        package='lidar_toolbox',
-        executable='proximity',
-        output='screen'
-    ),
+    Node(package="lidar_toolbox", executable="scan_transformer", output="screen"),
+    Node(package="lidar_toolbox", executable="proximity", output="screen"),
     ExecuteProcess(
         cmd=[
-            *['ros2', 'bag', 'record', '-o', RUN_VAR],
-            '/scan_transformed',
-            '/rover/base/velocity/get',
-            '/rover/base/halt'
+            *["ros2", "bag", "record", "-o", str(RUN_VAR)],
+            "/scan_transformed",
+            "/rover/base/velocity/get",
+            "/rover/base/halt",
         ],
-        cwd=str(PWD / 'var'),
-        output='screen'
+        cwd=str(PWD / "var"),
+        output="screen",
     ),
-
-    # Define an event handler to run the encode-video script on shutdown
-    # RegisterEventHandler(
-    #     OnShutdown(
-    #         on_shutdown=[
-    #             LogInfo(msg="Shutting down... Executing the encode-video script."),
-    #             ExecuteProcess(
-    #                 cmd=[
-    #                     str(PWD / 'scripts/runtime-bin/encode-video'),
-    #                     RUN_VAR + "/perception_images"
-    #                 ],
-    #                 cwd=str(PWD),
-    #                 shell=True,
-    #                 output='screen'
-    #             )
-    #         ]
-    #     )
-    # )
-
 ]
 
+
 # Define a custom shutdown callback function to use os.system
-def shutdown_callback(event, context):
+def shutdown_callback(*args, **kwargs):
     """Function to be called on shutdown."""
-    script_path = str(PWD / 'scripts/runtime-bin/encode-video')
-    os.system(f"{script_path} {RUN_VAR}/perception_images")
+    script_path = str(PWD / "scripts/runtime-bin/encode-video")
+    os.system(f"{script_path} {RUN_VAR}/perception_images {RUN_VAR}/perception.mp4")
     return [LogInfo(msg="Shutdown callback executed. Video encoding started.")]
 
-# Create the event handler for shutdown
-shutdown_handler = RegisterEventHandler(
-    OnShutdown(
-        on_shutdown=shutdown_callback
-    )
-)
 
-# Include the shutdown handler in the nodes list
-nodes.append(shutdown_handler)
-
+atexit.register(shutdown_callback)
 
 
 def generate_launch_description():
