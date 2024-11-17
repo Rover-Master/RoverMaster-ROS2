b158bd9281fccec959bbf6e394c3171a5868f5e7
Yuxuan Zhang <zyx@z-yx.cc>
2024-11-17 22:50:35 +0000

    update launch/rover.py: remove unnecessary nodes

--- launch/rover.py
+++ launch/rover.py
@@ -1,13 +1,5 @@
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
-from os import environ as env
-from pathlib import Path
-from datetime import datetime
-
-run_id = datetime.now().strftime("%Y%m%d_%H%M%S")
-PWD = Path(env["PWD"])
-RUN_VAR = PWD / "var" / run_id
 
 nodes = [
     Node(
@@ -28,33 +20,6 @@ nodes = [
             ("imu", "/rover/base/imu"),
             ("halt", "/rover/base/halt"),
         ],
-    ),
-    Node(
-        package="sllidar_ros2",
-        executable="sllidar_node",
-        parameters=[
-            {
-                "channel_type": "serial",
-                "serial_port": "/dev/rplidar",
-                "serial_baudrate": 115200,
-                "frame_id": "laser",
-                "inverted": False,
-                "angle_compensate": True,
-            }
-        ],
-        output="screen",
-    ),
-    Node(package="lidar_toolbox", executable="scan_transformer", output="screen"),
-    Node(package="lidar_toolbox", executable="proximity", output="screen"),
-    ExecuteProcess(
-        cmd=[
-            *["ros2", "bag", "record", "-o", str(RUN_VAR)],
-            "/scan_transformed",
-            "/rover/base/odometry",
-            "/rover/base/halt",
-        ],
-        cwd=str(PWD / "var"),
-        output="screen",
     ),
 ]
 
